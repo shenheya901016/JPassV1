aa0c3ce16e2b98b076946680fcf1e13bdedaf152
zCaviare <194108668@qq.com>
2019-10-16 09:50:31 +0800

    ipfs接口修改

--- test/test_ipfs.js
+++ test/test_ipfs.js
@@ -17,8 +17,8 @@ describe("test Ipfs", async function () {
             console.log(await MyIpfs.read(userJID));//读取该钱包地址在ipfs中存储的数据
             //console.log(await MyIpfs.write(
                 //'{"models":[{"id":"1","name":"模板1"}],"profile":[{"id":"1","name":"个人信息1"}],"project":[{"id":"1","name":"项目1"}]}',
-                //userJID,userSecret,operatorJID,operatorSecret));//data是决定传字符串还是object未决定 暂时传字符串  向ipfs中写入数据
-            //console.log(await MyIpfs.init(userJID,userSecret,operatorJID,operatorSecret));
+                //userJID,userSecret,operatorJID,operatorSecret));//data是决定传字符串还是object未决定 暂时传字符串  向ipfs中写入数据  会返回一个transaction参数提供给tra()
+            //console.log(await MyIpfs.init(userJID,userSecret,operatorJID,operatorSecret));//当判断用户钱包地址已被激活时执行 每个用户只会执行一次该方法
             //console.log(await MyIpfs.tra("699710DA3B50D7F13FD8981A098A6427564911E8C3A6B0F60DEB1DD3FC2E748C"));//判断向ipfs写入数据是否完成（同步是否完成）
         })
     })
