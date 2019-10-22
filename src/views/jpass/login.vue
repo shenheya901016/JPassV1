5e89505d6959ac052dfe4b6f61003dfa1bcce981
shenheya901016 <357924050@qq.com>
2019-10-22 11:03:48 +0800

    同步

--- src/views/jpass/login.vue
+++ src/views/jpass/login.vue
@@ -51,6 +51,8 @@
                 percentage: 0,
                 dialogVisible: false,
                 names: [],
+                operatorJID: "jHDbFiFZ6rfDjhfRnhD1ReCwY2erhpiYBS",//运营商钱包地址
+                operatorSecret: "ssxWidEVcs6bCtsVbfd7gMXUoRfMW",//运营商密钥
                 ruleForm: {
                     name: '',
                     password: '',
@@ -76,15 +78,6 @@
                     return {value: item, label: item};
                 });
             }
-            /* let transaction = await this.$myIpfs.write(   //data是决定传字符串还是object未决定 暂时传字符串  向ipfs中写入数据  会返回一个transaction参数提供给tra()
-                 '{"models":[{"id":"1","name":"模板1"}],"profile":[{"id":"1","name":"个人信息1"}],"project":[{"id":"1","name":"项目1"}]}',
-                 "j4M4AoSi522XxNpywfyBahmjzQihc4EegL", "sa9UcyBBD3A3JU3Ux3ZKcbNCxVw9h", "jHDbFiFZ6rfDjhfRnhD1ReCwY2erhpiYBS", "ssxWidEVcs6bCtsVbfd7gMXUoRfMW");
-             console.log(transaction);
-             let result=await this.$myIpfs.tra(transaction)
-             console.log(result);    //判断向ipfs写入数据是否完成（同步是否完成）
-             alert(result)*/
-            //console.log(await this.$myIpfs.bal("j4M4AoSi522XxNpywfyBahmjzQihc4EegL"));
-            //console.log(await this.$myIpfs.init("j4M4AoSi522XxNpywfyBahmjzQihc4EegL", "sa9UcyBBD3A3JU3Ux3ZKcbNCxVw9h", "jHDbFiFZ6rfDjhfRnhD1ReCwY2erhpiYBS", "ssxWidEVcs6bCtsVbfd7gMXUoRfMW"))
         },
         methods: {
             submitForm(formName) {
@@ -136,6 +129,7 @@
                     }
                     if(await this.$myIpfs.bal("j4M4AoSi522XxNpywfyBahmjzQihc4EegL") === "success"){
                    //if(await this.$myIpfs.bal("objKeyStore.wallets[0].address") === "success"){
+                        await this.$myIpfs.init( objKeyStore.wallets[0].address, secret, this.operatorJID,this.operatorSecret);
                         bal=true;
                     }else{
                         bal=false;
