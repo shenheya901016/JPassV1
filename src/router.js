93c686454e0f423bb2abce594c70c1e4c30c7f8e
shenheya901016 <357924050@qq.com>
2020-06-12 15:05:50 +0800

    模板调整 localstorage 增加登录信息

--- src/router.js
+++ src/router.js
@@ -97,14 +97,14 @@ export default new Router({
             name: "main",
             component: Main
         },
-        {
-            path: "/about",
-            name: "about",
-            // route level code-splitting
-            // this generates a separate chunk (about.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () =>
-                import(/* webpackChunkName: "about" */ "./views/About.vue")
-        }
+        // {
+        //     path: "/about",
+        //     name: "about",
+        //     // route level code-splitting
+        //     // this generates a separate chunk (about.[hash].js) for this route
+        //     // which is lazy-loaded when the route is visited.
+        //     component: () =>
+        //         import(/* webpackChunkName: "about" */ "./views/About.vue")
+        // }
     ]
 });
